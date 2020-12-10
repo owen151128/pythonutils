aa08a443e98989fbda9ed6b2cf2f6c9a750bf897
Sangmin Shim <owen151128@gmail.com>
2020-12-10 18:59:03 +0900

    Update argparse.py

--- argparse.py
+++ argparse.py
@@ -1,31 +1,27 @@
-# -*- coding: utf-8 -*-
-
+# -*- coding::utf-8 -*-
 
 from pyfiglet import Figlet
 
-import sys
-import os
-import argparse
 import time
 
-applicationName = 'APK Obfuscator'
-versionName = ' APK Obfuscator v1.0.0 '
-example = 'example : python apk_obfuscator.py -i absolute_input.apk -o absolute_obfuscated.apk'
+
+class EntryDescription:
+    def __init__(self, application_name, version_name, example_message):
+        self.application_name = application_name
+        self.version_name = f' {application_name} {version_name} '
+        self.example_message = f'example : python {__file__} {example_message}'
+
+    def print_entry_description(self):
+        result = Figlet(font='slant')
+        print(result.renderText(self.application_name))
+        print(f'{"=" * 80}\n{self.version_name:=^80}\n{"=" * 80}\n')
+        time.sleep(0.01)
 
 
-def main(argv):
-    result = Figlet(font='slant')
-    print(result.renderText(applicationName))
-    print(f'{"=" * 40}\n{versionName:=^40}\n{"=" * 40}\n')
-    time.sleep(0.01)
-    arg_parser = argparse.ArgumentParser(description=versionName, epilog=example)
-    arg_parser.add_argument('-i', metavar='target_apk', required=True, help='obfuscate target apk path')
-    arg_parser.add_argument('-o', metavar='obfuscated_apk', required=True, help='obfuscated output apk path')
-    args = arg_parser.parse_args()
-
-    print('i : ' + args.i)
-    print('b : ' + args.o)
+def main():
+    entry_description = EntryDescription('Apks to Apk', 'v0.0.1', '-i absolute_apks_dir -o absolute_path.apk')
+    entry_description.print_entry_description()
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
